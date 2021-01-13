2bc7de97009724424d1d16a1b071b8f6a19b73ce
evcp <emiliacalvacheponce@gmail.com>
2021-01-13 18:29:29 -0500

    HW03 Interations

--- assignments/Unit 1/HW03Interations/src/hw03interations/HW03Interations.java
+++ assignments/Unit 1/HW03Interations/src/hw03interations/HW03Interations.java
@@ -3,58 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ws05iterations;
+package hw03interations;
 
 /**
  *
  * @author Emilia
  */
-public class WS05Iterations {
+public class HW03Interations {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       
         int i;
         i=0;
         System.out.println("=== This is a while loop from 1 to 10===");
         while (i<10){
             System.out.println("i --" +(i+1));
             i++;
-          
         }
         
-        System.out.println("First 5 even numbers");
-        i=2;
-        while (i<=10){
-            System.out.println(i + "-- is even" );
-            i+=2;
-        }
-        
-        System.out.println("First 5 even numbers");
-        
-        int stop;
-        stop=20;
-        for(int j=1; j<stop;j+=2){
-            System.out.println("odd number--" +j);
-           
-        }
-       System.out.println("Multiplication Tables  : 7"); 
+      System.out.println("Multiplication Table : 10"); 
        int top=12;
        int product=0;
-       int table =7;
+       int table =10;
        for(int j =1; j<=top; j++ ){
            product = table * j;
-           System.out.println("7 * " + "=" + product);
-       }
-    }
-      
-   }
-      
+           System.out.println("10 * " + "=" + product);
+       }  
+       
+        System.out.println("Rest : 10 ");
+        int stop=10;
+        int rest=0;
+        for(int j=1; j<=stop;j++){
+            rest= stop - j;
+            System.out.println("10 -" +j + "=" + rest);
+        
+        
                 
-
-    
-    
-
+    }
+  }     
+}
